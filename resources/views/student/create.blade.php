fbf57fe9c7d72f831c712923e22ecdac29f08ad8
Kwizera Emmanuel <kwizeraemmanuel11@gmail.com>
2022-01-22 13:55:09 +0530

    laravel-crud

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>crud</title>
+    <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/fontawesome5-overrides.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Add-Another-Button.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Ludens-Users---25-After-Register.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/styles.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Table-With-Search-1.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Table-With-Search.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('') }}">
+    <link rel="stylesheet" href="{{ URL::asset('') }}">
+
+</head>
+
+<body>
+    <section class="clean-block clean-form dark h-100">
+        <div class="container">
+            <div class="block-heading" style="padding-top: 0px;">
+                <h1>REGISTER</h1>
+            </div>
+            <form action="index.html" method="post" enctype="multipart/form-data" role="form">
+                <div class="form-group mb-3"><label class="form-label">Name</label><input class="form-control" type="tel" placeholder="Name" name="phone" required=""></div>
+                <div class="form-group mb-3"><label class="form-label">Age</label><input class="form-control" type="tel" placeholder="Age" name="phone" required=""></div>
+                <div class="form-group mb-3"><label class="form-label">Course</label><input class="form-control" type="text" autocomplete="off" required="" placeholder="Course" name="city" title="Debes escribir el nombre de una ciudad"></div>
+                <hr style="margin-top: 30px;margin-bottom: 10px;">
+                <div style="text-align: right;">
+                    <div class="d-inline-block form-group mb-3"><a href="#"></a><button class="btn btn-primary d-block w-100" type="submit" style="background: var(--bs-blue);border-color: var(--bs-blue);"><i class="fas fa-save"></i>&nbsp;Save</button></div>
+                    <div class="d-inline-block form-group mb-3"><a href="#"></a><button class="btn btn-primary d-block w-100" type="submit" style="background: var(--bs-red);border-color: var(--bs-red);"><i class="icon ion-android-cancel"></i>&nbsp;Back</button></div>
+                </div>
+            </form>
+        </div>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/Table-With-Search.js"></script>
+</body>
+
+</html>
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -5,22 +5,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Laravel-Crud</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
-    <link rel="stylesheet" href="assets/css/Add-Another-Button.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-    <link rel="stylesheet" href="assets/css/Ludens-Users---25-After-Register.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" href="assets/css/Table-With-Search-1.css">
-    <link rel="stylesheet" href="assets/css/Table-With-Search.css">
+    <link rel="stylesheet" href="{{ URL::asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/fonts/fontawesome5-overrides.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Add-Another-Button.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Ludens-Users---25-After-Register.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/styles.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Table-With-Search-1.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/css/Table-With-Search.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('') }}">
+
 </head>
 
 <body>
     <div class="container">
-        <div class="col-md-12 search-table-col"><a href="register.html"><button class="btn btn-outline-primary text-truncate float-none float-sm-none add-another-btn" data-bss-hover-animate="pulse" type="button">Add Student<i class="fas fa-plus-circle edit-icon"></i></button></a>
+        <div class="col-md-12 search-table-col"><a href="{{ url('student/create') }}"><button class="btn btn-outline-primary text-truncate float-none float-sm-none add-another-btn" data-bss-hover-animate="pulse" type="button">Add Student<i class="fas fa-plus-circle edit-icon"></i></button></a>
             <div class="form-group pull-right col-lg-4"><input type="text" class="search form-control" placeholder="Search by typing here.."></div><span class="counter pull-right"></span>
             <div class="table-responsive table table-hover table-bordered results" style="margin-top: 5px;">
                 <table class="table table-hover table-bordered">
